ee788e77ef560fdea1e705f40aeedec36cf75f2c
EwaWagner <74456960+EwaWagner@users.noreply.github.com>
2022-07-13 21:21:52 +0100

    level mode in

--- higher_lower_game.py
+++ higher_lower_game.py
@@ -1,21 +1,41 @@
-from operator import truediv
 import random
-numb1 = random.randint(0,10)
-print(numb1)
+print("Chose your level (easy, medium, hard)")
+easy = 10
+medium = 100
+hard = 1000
+level = input()
 
-print("Say 1. higher or 2. lower")
+
+
+if level == "easy":
+    numb1 = random.randint(0,easy)
+    print(numb1)
+    numb2 = random.randint(0,easy)
+if level == "medium":
+    numb1 = random.randint(0,medium)
+    print(numb1)
+    numb2 = random.randint(0,medium)
+if level == "hard":
+    numb1 = random.randint(0,hard)
+    print(numb1)
+    numb2 = random.randint(0,hard)
+
+
+
+print("Is your next number, (1. higher) or (2. lower)")
 decision = input()
-numb2 = random.randint(0,10)
+
+
 
 if decision == "higher" or decision == "1":
     if numb1 < numb2:
         print("you are correct, the second number was", numb2)
     else:
         print("loserrrrrr, your number was", numb2)
-
-if decision == "lower" or decision == "2":
+elif decision == "lower" or decision == "2":
     if numb1 > numb2:
         print("you are correct, the second number was", numb2)
     else:
         print("loserrrrrr, your number was", numb2)
-
+else:
+    exit()
